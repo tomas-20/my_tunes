4352af203fc99c6ca6e3473f4915ebfd9656aa5a
tomas-20 <tacuna20@stuy.edu>
2021-10-22 11:39:26 -0400

    created the less_than function for comparing song_lists

--- song_list.c
+++ song_list.c
@@ -1,5 +1,20 @@
+#import <string.h>
 struct song_list {
   char *name;
   char *artist;
   struct song_list *next;
  };
+ int less_than(struct song_list *a, struct song_list *b) {
+   int name_cmp = strcasecmp(a->name, b->name);
+   if (name_cmp < 0) {
+     return 1;
+   }
+   if (name_cmp > 0) {
+     return 0;
+   }
+   int artist_cmp = strcasecmp(a->artist, b->artist);
+   if (artist_cmp < 0) {
+     return 1;
+   }
+   return 0;
+ }
--- song_list.h
+++ song_list.h
@@ -3,3 +3,4 @@ struct song_list {
   char *artist;
   struct song_list *next;
  };
+ int less_than(struct song_list *, struct song_list *);
